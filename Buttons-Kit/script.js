47e4b38ce008003ec497ba4de88f0db54613421b
Waffen <waffen.sultanofc@gmail.com>
2023-08-03 14:50:37 +0800

    #

---
+++ Buttons-Kit/script.js
@@ -0,0 +1,6 @@
+const sort = document.getElementById('sort')
+const sortNav = document.getElementById('sortNav')
+
+sort.addEventListener('click', () => {
+    sortNav.classList.toggle('hidden')
+})
